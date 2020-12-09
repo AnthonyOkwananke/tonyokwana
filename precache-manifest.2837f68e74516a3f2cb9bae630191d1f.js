ea992763c537758148cb97cecf6c2c0e28d117c4
AnthonyOkwananke <57677240+AnthonyOkwananke@users.noreply.github.com>
2020-12-09 11:19:37 -0700

    Updates

---
+++ precache-manifest.2837f68e74516a3f2cb9bae630191d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49883e6411ab2357b5a4066542c17571",
+    "url": "/tonyokwana/index.html"
+  },
+  {
+    "revision": "4ae8682706876759b75f",
+    "url": "/tonyokwana/static/css/main.58456e87.chunk.css"
+  },
+  {
+    "revision": "f89126d6c74f357f3c83",
+    "url": "/tonyokwana/static/js/2.56a785bd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/tonyokwana/static/js/2.56a785bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae8682706876759b75f",
+    "url": "/tonyokwana/static/js/main.68f38bf3.chunk.js"
+  },
+  {
+    "revision": "a189a803053fd3d382a4",
+    "url": "/tonyokwana/static/js/runtime-main.5d5cd576.js"
+  }
+]);
