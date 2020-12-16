617fa7657262ea587f59c9de7010ff69d88a4d1e
AnthonyOkwananke <57677240+AnthonyOkwananke@users.noreply.github.com>
2020-12-16 13:19:02 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tonyokwana/precache-manifest.2837f68e74516a3f2cb9bae630191d1f.js"
+  "/tonyokwana/precache-manifest.988cb8aa625cf647fbbea021fa3ab5ef.js"
 );
 
 self.addEventListener('message', (event) => {
